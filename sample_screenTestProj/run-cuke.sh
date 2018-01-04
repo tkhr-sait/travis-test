1461dd06c3517e2ef0cb0ae7b9ecef1d1f3b33bc
tkhr-sait <tkhr-sait@users.noreply.github.com>
2018-01-04 22:44:00 +0900

    chore(*):ci in docker test3

--- sample_screenTestProj/run-cuke.sh
+++ sample_screenTestProj/run-cuke.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-java -Djava.awt.headless=true -Dselenide.browser=chrome -Dremote=http://hub:4444/wd/hub -cp 'target/scenario-test-0.0.1-SNAPSHOT-jar-with-dependencies.jar' cucumber.api.cli.Main  src/ --glue scenario.test
+java -Djava.awt.headless=true -Dselenide.browser=chrome -Dremote=http://127.0.0.1:4444/wd/hub -cp 'target/scenario-test-0.0.1-SNAPSHOT-jar-with-dependencies.jar' cucumber.api.cli.Main  src/ --glue scenario.test
 
--- sample_screenTestProj/src/main/resources/scenario/test/SwaggerEditor.feature
+++ sample_screenTestProj/src/main/resources/scenario/test/SwaggerEditor.feature
@@ -2,7 +2,7 @@ Feature: SwaggerEditorでgitユーザの管理を行いたい。
  操作者は、エディタ画面での操作者を把握するために、ユーザ管理を行う必要がある。
 
 Scenario:ユーザの登録
- Given "http://in-house-swagger:9700/editor" URLを開く
+ Given "http://127.0.0.1:9700/editor" URLを開く
   When "10_editor" スクリーンショットをとる
   When "ace-editor" に "#これは\n#コメントです\n" を入力する
   When "11_editorarea" 部品 "pane1" のスクリーンショットをとる
